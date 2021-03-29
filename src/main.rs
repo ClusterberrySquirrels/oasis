d2606cf5b59aa5e80f7b29cb3756aa9e12e1b7f8
Matt Hurt <mhurt@msudenver.edu>
2021-03-29 17:12:53 -0600

    update project after commit issue in another branch

---
+++ src/main.rs
@@ -0,0 +1,111 @@
+extern crate tera;
+use actix_web::{HttpServer, App, web, HttpResponse, Responder};
+use tera::{Tera, Context};
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize)]
+struct Post {
+	title: String,
+	link: String,
+	author: String,
+}
+
+#[derive(Deserialize, Debug)]
+struct User {
+    username: String,
+    email: String,
+    password: String,
+}
+
+#[derive(Deserialize, Debug)]
+struct LoginUser {
+    username: String,
+    password: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct Submission {
+    title: String,
+    link: String,
+}
+
+async fn index(tera: web::Data<Tera>) -> impl Responder {
+    let mut data = Context::new();
+
+    let posts = [
+        Post {
+            title: String::from("This is the first link"),
+            link: String::from("https://example.com"),
+            author: String::from("Bob")
+        },
+        Post {
+            title: String::from("This is the second Link"),
+            link: String::from("https://example.com"),
+            author: String::from("Alice")
+        },
+    ];
+
+    data.insert("title", "index");
+    data.insert("posts",&posts);
+
+    let rendered = tera.render("index.html", &data).unwrap();
+    HttpResponse::Ok().body(rendered)
+}
+
+async fn signup(tera: web::Data<Tera>) -> impl Responder {
+    let mut data = Context::new();
+    data.insert("title", "Sign Up");
+
+    let rendered = tera.render("signup.html", &data).unwrap();
+    HttpResponse::Ok().body(rendered)
+}
+
+async fn process_signup(data: web::Form<User>) -> impl Responder {
+    println!("{:?}", data);
+    HttpResponse::Ok().body(format!("Successfully saved user: {}", data.username))
+}
+
+async fn login(tera: web::Data<Tera>) -> impl Responder {
+    let mut data = Context::new();
+    data.insert("title", "Login");
+
+    let rendered = tera.render("login.html", &data).unwrap();
+    HttpResponse::Ok().body(rendered)
+}
+
+async fn process_login(data: web::Form<LoginUser>) -> impl Responder {
+    println!("{:?}", data);
+    HttpResponse::Ok().body(format!("Logged in: {}", data.username))
+}
+
+async fn submission(tera: web::Data<Tera>) -> impl Responder {
+    let mut data = Context::new();
+    data.insert("title", "Submit a Post");
+
+    let rendered = tera.render("submission.html", &data).unwrap();
+    HttpResponse::Ok().body(rendered)
+}
+
+async fn process_submission(data: web::Form<Submission>) -> impl Responder {
+    println!("{:?}", data);
+    HttpResponse::Ok().body(format!("Posted submission: {}", data.title))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        let tera = Tera::new("templates/**/*").unwrap();
+        App::new()
+            .data(tera)
+            .route("/", web::get().to(index))
+            .route("/signup", web::get().to(signup))
+            .route("/signup", web::post().to(process_signup))
+            .route("/login", web::get().to(login))
+            .route("/login", web::post().to(process_login))
+            .route("/submission", web::get().to(submission))
+            .route("/submission", web::post().to(process_submission))
+    })
+        .bind("127.0.0.1:8000")?
+        .run()
+        .await
+}
