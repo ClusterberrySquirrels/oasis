19b572687b6266cd0ab67b01efb522c836e647e8
Matt Hurt <mhurt@msudenver.edu>
2021-03-31 13:06:34 -0600

    process_signup() function has a insert error where the trait is not implemented for 'User'

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,31 @@
 extern crate tera;
+#[macro_use]
+extern crate diesel;
+
+pub mod schema;
+pub mod models;
+
 use actix_web::{HttpServer, App, web, HttpResponse, Responder};
 use tera::{Tera, Context};
 use serde::{Serialize, Deserialize};
+use diesel::prelude::*;
+use diesel::pg::PgConnection;
+use dotenv::dotenv;
+use models::{User, NewUser};
 
 #[derive(Serialize)]
 struct Post {
-	title: String,
-	link: String,
-	author: String,
+    title: String,
+    link: String,
+    author: String,
 }
 
-#[derive(Deserialize, Debug)]
-struct User {
-    username: String,
-    email: String,
-    password: String,
-}
+// #[derive(Deserialize, Debug)]
+// struct User {
+//     username: String,
+//     email: String,
+//     password: String,
+// }
 
 #[derive(Deserialize, Debug)]
 struct LoginUser {
@@ -29,6 +39,16 @@ struct Submission {
     link: String,
 }
 
+fn establish_connection() -> PgConnection {
+    dotenv().ok();
+
+    let database_url = std::env::var("DATABASE_URL")
+        .expect("DATABASE_URL must be set");
+
+    PgConnection::establish(&database_url)
+        .expect(&format!("Error connecting to {}", database_url))
+}
+
 async fn index(tera: web::Data<Tera>) -> impl Responder {
     let mut data = Context::new();
 
@@ -36,17 +56,17 @@ async fn index(tera: web::Data<Tera>) -> impl Responder {
         Post {
             title: String::from("This is the first link"),
             link: String::from("https://example.com"),
-            author: String::from("Bob")
+            author: String::from("Nutrition-Tracker"),
         },
         Post {
             title: String::from("This is the second Link"),
             link: String::from("https://example.com"),
-            author: String::from("Alice")
+            author: String::from("Other cool app"),
         },
     ];
 
     data.insert("title", "index");
-    data.insert("posts",&posts);
+    data.insert("posts", &posts);
 
     let rendered = tera.render("index.html", &data).unwrap();
     HttpResponse::Ok().body(rendered)
@@ -61,6 +81,15 @@ async fn signup(tera: web::Data<Tera>) -> impl Responder {
 }
 
 async fn process_signup(data: web::Form<User>) -> impl Responder {
+    use schema::users;
+
+    let connection = establish_connection();
+
+    diesel::insert_into(users::table)
+        .values(data.into_inner())// FIXME: the trait `diesel::Insertable<users::table>` is not implemented for `User`
+        .get_result::<User>(&connection)
+        .expect("Error registering user.");
+
     println!("{:?}", data);
     HttpResponse::Ok().body(format!("Successfully saved user: {}", data.username))
 }
